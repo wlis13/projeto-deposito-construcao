1a4b6cb14498cac47750e30ea2fcb549e48f2164
wlisses <wlissesfernando285@gmail.com>
2022-12-03 19:18:25 -0300

    feitas mais algumas estilizações

--- script.js
+++ script.js
@@ -29,62 +29,62 @@ const products =
   [
     {
       name: 'carrinho de mão',
-      price: 211.99,
+      price: `R$ 211,99`,
       image: './images/carrinho-de-mão.jpeg',
     },
     {
       name: 'colher de pedreiro',
-      price: 19.99,
+      price: 'R$ 19,99',
       image: './images/colher-de-pedreiro.jpeg',
     },
     {
       name: 'furadeira Boch',
-      price: 239.99,
+      price: 'R$ 239,99',
       image: './images/furadeira.jpeg',
     },
     {
       name: 'trenna Vonder',
-      price: 23.99,
+      price: 'R$ 23,99',
       image: './images/trenna.jpeg',
     },
     {
       name: 'martelo Ferreiro',
-      price: 29.99,
+      price: 'R$ 29,99',
       image: './images/imagen-de-martelo.jpeg',
     },
     {
       name: 'piso-incenor-66180 58x58',
-      price: 69.99,
+      price: 'R$ 69,99',
       image: './images/piso-incenor-66180 58x58-1.jpg',
     },
     {
       name: 'piso-pointer-cantabria-gris 45x90',
-      price: 121.99,
+      price: 'R$ 121,99',
       image: './images/piso-pointer-cantabria-gris 45x90-1.jpg',
     },
     {
       name: 'porcelanato-bianco-lux 90x90',
-      price: 111.98,
+      price: 'R$ 111,98',
       image: './images/porcelanato-biancogres -lux-90x90-1.jpg',
     },
     {
       name: 'porcelanato-branco-PO 90x90',
-      price: 29.99,
+      price: 'R$ 29,99',
       image: './images/porcelanato-branco-PO.jpeg',
     },
     {
       name: 'porcelanato-PO-lux 120x60',
-      price: 125.98,
+      price: 'R$ 125,98',
       image: './images/porcelanato.jpeg',
     },
     {
       name: 'Ferragen 5/16',
-      price: 24.98,
+      price: 'R$ 24,99',
       image: './images/ferragen01.jpeg',
     },
     {
       name: 'ferragens montada 5/16',
-      price: 45.69,
+      price: 'R$ 45,99',
       image: './images/ferragen02.jpeg',
     },
   ];
