870b1448c34184c11463bf2a67ac5bfa01dde8f6
wlisses <wlissesfernando285@gmail.com>
2022-12-04 12:57:38 -0300

    inserindo nome, imagen, e proço dos produtos escolhidos

--- scriptUserProducts.js
+++ scriptUserProducts.js
@@ -4,10 +4,18 @@ const totalValue = getProductsUser;
 const userProducts = document.querySelector('.user-products');
 
 for (let index = 0; index < totalValue.length; index += 1) {
+  const divCart = document.createElement('div');
+  divCart.classList.add('div-cart');
   const named = document.createElement('p');
   named.innerText = totalValue[index].objectProducts.nameProduct;
-  named.classList.add('named-product-user');
-  userProducts.appendChild(named)
+  const imaged = document.createElement('img');
+  imaged.src = totalValue[index].objectProducts.imageProduct;
+  const priced = document.createElement('h4');
+  priced.innerText = totalValue[index].objectProducts.priceProduct;
+  divCart.appendChild(named)
+  divCart.appendChild(imaged)
+  divCart.appendChild(priced)
+  userProducts.appendChild(divCart)
 
 }
 
